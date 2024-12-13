8497ad8463a4280c2d46b39e08dca92368301244
marco <marcoramirezmail@gmail.com>
2024-12-13 15:42:22 -0800

    product styling

--- homework1/src/components/product.js
+++ homework1/src/components/product.js
@@ -6,30 +6,34 @@ export default function Product() {
     {
       id: 1,
       img: "/imgs/img1.jpg",
-      name: "Product 1",
+      name: "Schlagenheim",
       price: 100,
     },
     {
       id: 2,
       img: "/imgs/img2.jpg",
-      name: "Product 2",
+      name: "What Burns Never Returns",
       price: 200,
     },
     {
       id: 3,
       img: "/imgs/img3.jpg",
-      name: "Product 3",
+      name: "Bridge Over Troubled Water",
       price: 300,
     },
   ];
   return (
-    <div className={styles.product}>
+    <div className={styles.products}>
       {products.map((product) => (
-        <div key={product.id}>
-          <Image width={50} height={50} src={product.img}></Image>
-          <div>{product.name}</div>
-          <div>{product.price}</div>
-          <h3>test</h3>
+        <div className={styles.product} key={product.id}>
+          <Image
+            width={200}
+            height={200}
+            src={product.img}
+            alt={product.name}
+          />
+          <h3>{product.name}</h3>
+          <div>${product.price}</div>
         </div>
       ))}
     </div>
